e53d06b50ab23dac7363080b8a1f5dff8d5e2508
Bruno La Porta Welausen <brunolpw@gmail.com>
2024-05-10 18:04:38 -0300

    HandDto

---
+++ src/main/java/com/brunolpw/deckofcards/dtos/HandDto.java
@@ -0,0 +1,16 @@
+package com.brunolpw.deckofcards.dtos;
+
+import java.util.List;
+import java.util.UUID;
+
+import com.google.gson.annotations.SerializedName;
+
+public record HandDto(
+        UUID handId,
+        boolean success,
+        @SerializedName("deck_id")
+        String deckId,
+        List<CardDto> cards
+) {
+
+}
