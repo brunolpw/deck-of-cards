908c75fb47ff975197e692282fdbb011a5b2293c
Bruno La Porta Welausen <brunolpw@gmail.com>
2024-05-10 18:07:46 -0300

    Alteração do cliente feign

--- src/main/java/com/brunolpw/deckofcards/services/DeckService.java
+++ src/main/java/com/brunolpw/deckofcards/services/DeckService.java
@@ -4,37 +4,42 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.brunolpw.deckofcards.exceptions.DeckRemainingException;
 import com.brunolpw.deckofcards.models.Deck;
 import com.brunolpw.deckofcards.repositories.DeckRepository;
-import com.google.gson.Gson;
-
+import com.brunolpw.deckofcards.utils.Utils;
 import jakarta.transaction.Transactional;
 
 @Service
 public class DeckService {
-    private final DeckServiceClient _deckServiceClient;
+    private final DeckOfCardsServiceClient _deckOfCardsServiceClient;
     private final DeckRepository _deckRepository;
 
-    public DeckService(DeckServiceClient deckServiceClient, DeckRepository deckRepository) {
-        _deckServiceClient = deckServiceClient;
+    public DeckService(DeckOfCardsServiceClient deckOfCardsServiceClient, DeckRepository deckRepository) {
+        _deckOfCardsServiceClient = deckOfCardsServiceClient;
         _deckRepository = deckRepository;
     }
 
     @Transactional
     public Deck getNewDeck() {
-        var json = _deckServiceClient.getNewDeck();
+        var json = _deckOfCardsServiceClient.getNewDeck();
 
         if (json == null) {
             return null;
         }
 
-        var deck = convertJsonToDeck(json);
+        var deck = Utils.convertJsonToObject(json, Deck.class);
 
         _deckRepository.save(deck);
 
         return deck;
     }
     
+    @Transactional
+    public Deck getDeck(String deckId) {
+        return _deckRepository.findById(deckId).orElse(null);
+    }
+
     @Transactional
     public List<Deck> getAllDecks() {
         return _deckRepository.findAll();
@@ -42,23 +47,36 @@ public class DeckService {
     
     @Transactional
     public Deck shufflDeck(String deckId) {
-        var json = _deckServiceClient.shuffleDeck(deckId);
+        var json = _deckOfCardsServiceClient.shuffleDeck(deckId);
 
         if (json == null) {
             return null;
         }
-        
-        var deck = convertJsonToDeck(json);
+
+        var deck = Utils.convertJsonToObject(json, Deck.class);
 
         _deckRepository.save(deck);
 
         return deck;
     }
     
-    private Deck convertJsonToDeck(String json) {
-        var gson = new Gson();
-        var deck = gson.fromJson(json, Deck.class);
-        
-        return deck;
+    @Transactional
+    public Deck update(Deck deck) {
+        return _deckRepository.save(deck);
     }
+
+    @Transactional
+    public Deck remainingCards(String deckId, int count) {
+        var deck = getDeck(deckId);
+
+        if (deck == null) {
+            return null;
+        }
+
+        if (deck.getRemaining() < count) {
+            throw new DeckRemainingException(deck.getRemaining());
+        }
+
+        return update(new Deck(deck.getDeckId(), deck.getRemaining() - count, deck.isSuccess(), deck.isShuffled()));
+    } 
 }
